54ce9f1f881d60ec2248e0b5f6d6a751b79c97d7
Challengerfrom35yearsold <124878663+Challengerfrom35yearsold@users.noreply.github.com>
2023-05-01 13:46:59 +0000

    [Finish]4-20

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,6 +10,7 @@ class FavoritesController < ApplicationController
   def destroy
     post_image = PostImage.find(params[:post_image_id])
     favorite = current_user.favorites.find_by(post_image_id: post_image.id)
+    #post_image_id:が post_image.idのデータ
     favorite.destroy
     redirect_to post_image_path(post_image)
   end
