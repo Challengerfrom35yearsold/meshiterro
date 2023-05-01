aa5381d3add87455c525ee61d7d59daa82a82b37
Challengerfrom35yearsold <124878663+Challengerfrom35yearsold@users.noreply.github.com>
2023-05-01 16:35:38 +0000

    [Finish]4-22

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top]
   #before_actionメソッドは、このコントローラが動作する前に実行される
   #authenticate_userメソッドは、ログイン認証されていなければ、ログイン画面へリダイレクトする
-  #exceptは指定したアクションをbefore_actionの対象から外す。今回の場合はtopページのみアクセス可能と
+  #exceptは指定したアクションをbefore_actionの対象から外す。今回の場合はtopページのみアクセス可能となる
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
